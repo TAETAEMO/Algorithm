dbd8782c85bb4250e38053777650e4307cf963fa
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2023-09-15 23:46:15 +0900

    Time: 65 ms (61.55%), Space: 42.3 MB (70.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -29,7 +29,24 @@ var twoSum = function(nums, target) {
 //         // complement가 아직 저장되지 않았다면 현재 숫자와 인덱스를 저장
 //         numToIndexMap[nums[i]] = i;
 //     }
-
-//     // 솔루션이 반드시 존재하므로 여기까지 오면 안됩니다.
 //     return null;
 // }
+
+
+// function twoSum(nums, target) {
+//     const numToIndexMap = new Map(); // 숫자와 해당 인덱스를 저장할 Map 객체
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const complement = target - nums[i]; // 현재 숫자와 합해서 target이 되는 수
+//         if (numToIndexMap.has(complement)) {
+//             // complement가 이미 저장된 숫자라면 해당 숫자의 인덱스와 현재 인덱스를 반환
+//             return [numToIndexMap.get(complement), i];
+//         }
+//         // complement가 아직 저장되지 않았다면 현재 숫자와 인덱스를 Map에 저장
+//         numToIndexMap.set(nums[i], i);
+//     }
+//     return null;
+// }
+
+// * in 연산자
+// * has 메서드
