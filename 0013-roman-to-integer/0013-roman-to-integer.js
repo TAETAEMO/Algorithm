03a9fd9a2f45ae6daec32d00e02301f4f4681279
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2023-09-18 23:15:27 +0900

    Time: 124 ms (24.38%), Space: 46.6 MB (83.74%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  const romanToIntMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+    let prevValue = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanToIntMap[s[i]];
+
+        if (currentValue < prevValue) {
+            result -= currentValue;
+        } else {
+            result += currentValue;
+        }
+
+        prevValue = currentValue;
+    }
+
+    return result;
+    
+};
+
+//다시풀기
