bfe4051b11fbeeef9bae8cbbf1a546bfcfb050db
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2023-09-30 23:55:50 +0900

    Time: 60 ms (49.3%), Space: 44.5 MB (18.03%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -22,3 +22,34 @@ var mergeTwoLists = function(list1, list2) {
         return list2;
     }
 };
+
+/*
+function mergeTwoLists(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+
+    let mergedHead = new ListNode();
+    let current = mergedHead;
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    if (l1 !== null) {
+        current.next = l1;
+    }
+
+    if (l2 !== null) {
+        current.next = l2;
+    }
+
+    return mergedHead.next;
+}
+*/
