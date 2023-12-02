616f0cc403056a20735cb28902c59529e71d48fa
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2023-12-02 23:37:01 +0900

    Time: 86 ms (65.75%), Space: 47.4 MB (54.59%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -28,3 +28,23 @@ var addTwoNumbers = function (l1, l2) {
   }
   return node.next;
 };
+
+//오답
+/*var addTwoNumbers = function (l1, l2) {
+  const node = new ListNode();
+  let tmpNode = node;
+  let carry = 0;
+  while (l1 || l2 || carry) {
+    const left = l1 ? l1.val : 0;
+    const right = l2 ? l2.val : 0;
+    let sum = left + right + carry;
+    const value = sum < 10 ? sum : sum % 10;
+    carry = sum < 10 ? 0 : 1;
+    tmpNode.val = value;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+    tmpNode.next = new ListNode();
+    tmpNode = tmpNode.next;
+  }
+  return node;
+};*/
