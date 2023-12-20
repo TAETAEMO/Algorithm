8b677ebdf3561bfde35910d5581b2065d0765f79
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2023-12-20 23:21:36 +0900

    Time: 50 ms (92.99%), Space: 42.4 MB (83.70%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    const swap = (arr, a, b) => [ arr[a], arr[b] ] = [ arr[b], arr[a] ]
+    
+    let i = nums.length -2
+    while(0 <= i && nums[i] >= nums[i+1]) i--
+    
+    if (0 <= i) {
+        let j = nums.length-1
+        while(nums[i] >= nums[j]) j--
+        swap(nums, i, j)
+    }
+    nums.push(...nums.splice(i+1).reverse())
+};
