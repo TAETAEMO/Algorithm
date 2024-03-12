509928ff28c3dc8b113bdc03e0a857028a4fd617
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2024-03-12 21:52:49 +0900

    Time: 59 ms (12.86%), Space: 48.8 MB (26.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let cache = {};
+    
+    let howManyWays = function(n) {
+        if (n in cache) {
+            return cache[n];
+        }
+
+        if (n < 4) {
+            cache[n] = n;
+            return cache[n];
+        }
+        
+        cache[n] = (howManyWays(n - 1) + howManyWays(n - 2));
+        return cache[n];
+    }
+    
+    return howManyWays(n);
+};
