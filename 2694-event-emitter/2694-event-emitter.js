75bdd6a27736450e57e46b74266ab425ed21aabf
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2024-05-03 18:51:28 +0900

    Time: 48 ms (86.54%), Space: 50.1 MB (91.74%) - LeetHub

---
+++ 2694-event-emitter/2694-event-emitter.js
@@ -0,0 +1,40 @@
+class EventEmitter {
+    constructor() {
+        this.map = new Map();
+    }
+
+    subscribe(event, cb) {
+        if (!this.map.has(event)) {
+            this.map.set(event, []);
+        }
+
+        const arr = this.map.get(event);
+        arr.push(cb);
+
+        return {
+            unsubscribe: () => {
+                const index = arr.indexOf(cb);
+                if (index !== -1) {
+                    arr.splice(index, 1);
+                }
+            }
+        };
+    }
+
+    emit(event, args = []) {
+        const callbacks = this.map.get(event) || [];
+        return callbacks.map(callback => callback(...args));
+    }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
