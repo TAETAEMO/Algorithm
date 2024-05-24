9dddcb705f757cef1a39b9fd762a329c32c38cd4
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2024-05-24 23:47:12 +0900

    Time: 57 ms (67.49%), Space: 49.3 MB (39.40%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+function debounce(fn, t) {
+  let timerId;
+
+  return function (...args) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      fn(...args);
+    }, t);
+  };
+}
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
