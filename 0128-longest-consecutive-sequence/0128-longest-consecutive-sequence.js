a2d68065b6929319c98a4e66a2c9e880a8908106
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2024-07-04 22:28:42 +0900

    Time: 1231 ms (17.46%), Space: 72.7 MB (57.63%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+  if (nums == null || nums.length === 0) return 0;
+  const set = new Set(nums);
+  let longest = 0;
+  for (let num of nums) {
+    if (!set.has(num - 1)) {
+      let count = 0;
+      while (set.has(count+num)) {
+        count++;
+      }
+      longest = Math.max(longest,count);
+    }
+  }
+  return longest;
+};
