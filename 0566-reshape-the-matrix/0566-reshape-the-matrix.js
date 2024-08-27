6f049062066d95c5f99f715b4a2b622a330d108a
ANTAEGUN <104331117+TAETAEMO@users.noreply.github.com>
2024-08-27 23:16:02 +0900

    Time: 82 ms (33.07%), Space: 54.2 MB (58.96%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function(mat, r, c) {
+  if(!mat || !mat.length) return mat
+  const m = mat.length
+  const n = mat[0].length
+  if(m*n < r*c || m*n > r*c) return mat
+  
+  const arr = new Array(r).fill(0).map(_=> new Array(c).fill(0))
+  let row=0, col=0
+  for(let R=0;R<mat.length;R++){
+    for(let C=0;C<mat[R].length;C++){
+      if(col>= arr[row].length){
+        col = 0
+        row++
+      }
+      arr[row][col++] = mat[R][C]
+    }
+  }
+  return arr
+};
